6a121f0aaba1c76bac1cd0f96574ed5ff83cc985
Derrick Kisio <derrickkisio@gmail.com>
2025-11-19 04:46:28 +0300

    live

---
+++ terraform-conf/app.py
@@ -0,0 +1,15 @@
+import time
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def get_current_time():
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    return f"The current time is : {current_time}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0", port=8080)
